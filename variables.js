9a17c8385d71d50f4756b196ab66482957273e7b
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-01-10 20:18:34 -0700

    Add anotations to numbers

--- variables.js
+++ variables.js
@@ -2,8 +2,11 @@ var name = "Olivia"
 var petName = "Chama"
 var favoriteColor = "green"
 
+//Variable age declared
 var age = 26
+//Variable petNumber declared
 var petNumber = 1
+//Variable siblings declared
 var siblings = 1
 
 var pet = true
